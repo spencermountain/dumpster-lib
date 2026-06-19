ad8f745c11a0bd018e03d92af0be501e272842f2
spencer kelly <spencerkelly86@gmail.com>
2026-06-19 15:15:37 -0400

    ready

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import Pool from './pool/Pool.js'
 import defaults from './lib/defaults.js'
 
 // ok guess we're gonna do this...
-const run = function (opts) {
+const dumpster = function (opts) {
   opts = Object.assign({}, defaults, opts)
   let pool = new Pool(opts)
   pool.start()
-  return new Promise((resolve, reject) => {
-    pool.on('end', () => {
-      resolve()
-    })
-    pool.on('error', (err) => {
-      reject(err)
-    })
+  // return new Promise((resolve, reject) => {
+  pool.on('end', () => {
+    resolve()
   })
+  pool.on('error', (err) => {
+    reject(err)
+  })
+  // pool.on('chunk', (chunk) => {
+  //   console.log('chunks', chunk.length)
+  //   onChunk(chunk)
+  // })
+  // })
+  return pool
 }
-export default run
+export default dumpster
--- src/pool/Pool.js
+++ src/pool/Pool.js
@@ -44,13 +44,15 @@ class Pool extends EventEmitter {
       const worker = new Worker(file, info)
       // receive status of each worker, when requested
       worker.on('message', (msg) => {
+        // update the status of this worker
         if (msg.status) {
           this.status[msg.status.index] = msg.status
         }
+        // if we have a result, collect it
         if (msg.result) {
-          // collect this result
-          console.log('msg', this.results.length, 'of', this.opts.chunkSize)
-          this.results.push(msg.result)
+          // console.log('msg', this.results.length, 'of', this.opts.chunkSize)
+          this.results.push(msg.result.body)
+          // if we've reached the chunk size, emit the chunk
           if (this.results.length >= this.opts.chunkSize) {
             this.emit('chunk', this.results)
             this.results = []
@@ -69,8 +71,11 @@ class Pool extends EventEmitter {
   stop() {
     console.log('cleaning up...')
     clearInterval(this.heartbeat)
-    // this.workers.forEach(w => w.emit('exit'))
-    // this.emit('exit');
+    // drain any remaining results
+    if (this.results.length > 0) {
+      this.emit('chunk', this.results)
+      this.results = []
+    }
     this.emit('end')
     this.removeAllListeners()
     // log some stats
